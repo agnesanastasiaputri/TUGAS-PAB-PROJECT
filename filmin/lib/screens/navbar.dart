39c341e6df6ce82b3fbb24bba0a01bff456a85a7
ChandraSaputra2226250093 <145772827+ChandraSaputra2226250093@users.noreply.github.com>
2023-12-04 18:37:08 +0700

    navbar

--- filmin/lib/screens/navbar.dart
+++ filmin/lib/screens/navbar.dart
@@ -14,42 +14,47 @@ class NavBar extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           UserAccountsDrawerHeader(
-            accountName: Text('Chandra'), 
-            accountEmail: Text('Saputra@gmail.com'),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                child: Image.asset(
-                  'images/logo.jpg',
-                  width: 90,
-                  height: 90,
-                  fit: BoxFit.cover,
+              accountName: Text('Chandra'),
+              accountEmail: Text('Saputra@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                  child: Image.asset(
+                    'images/logo.jpg',
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
                   ),
+                ),
               ),
-            ),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 19, 17, 17)
-          )),
+              decoration:
+                  BoxDecoration(color: Color.fromARGB(255, 19, 17, 17))),
           ListTile(
-            iconColor: Colors.white,
-            leading: Icon(Icons.home),
-            title: Text('Halaman Depan'),
-            textColor: Colors.white,
-            onTap: () => null
-          ),
+              iconColor: Colors.white,
+              leading: Icon(Icons.home),
+              title: Text('Halaman Depan'),
+              textColor: Colors.white,
+              onTap: () => {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()))
+                  }),
           ListTile(
-            iconColor: Colors.white,
-            leading: Icon(Icons.star),
-            title: Text('Rekomendasi Film'),
-            textColor: Colors.white,
-            onTap: () => null
-          ),
+              iconColor: Colors.white,
+              leading: Icon(Icons.star),
+              title: Text('Rekomendasi Film'),
+              textColor: Colors.white,
+              onTap: () => {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => RateScreen()))
+                  }),
           ListTile(
-            iconColor: Colors.white,
-            leading: Icon(Icons.favorite),
-            title: Text('Favorit'),
-            textColor: Colors.white,
-            onTap: () => null
-          ),
+              iconColor: Colors.white,
+              leading: Icon(Icons.favorite),
+              title: Text('Favorit'),
+              textColor: Colors.white,
+              onTap: () => {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => FavoriteScreen()))
+                  }),
           ListTile(
             iconColor: Colors.white,
             leading: Icon(Icons.person),
