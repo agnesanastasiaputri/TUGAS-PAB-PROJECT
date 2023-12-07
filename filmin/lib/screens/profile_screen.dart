f37fadc4d4354bd9f372d7c8987b1e8dd6b71bd1
NelsonSaputraEdika <nelsonsaputraedika_2226250034@mhs.mdp.ac.id>
2023-12-07 22:53:21 +0700

    Profile Screen

--- filmin/lib/screens/profile_screen.dart
+++ filmin/lib/screens/profile_screen.dart
@@ -8,138 +8,46 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  String fullName = 'Nelson Saputra Edika';
-  String userName = 'nelson';
-  String email = 'nelsonsaputraedika_2226250034@mhs.mdp.ac.id';
-  String aboutMe =
-
-      'shwaygwuagwuaudbcuabdcuhbacajwkajmwknakwndkjbawjdbhjawbdjhvawudvjavwdjhvawjhdvajhwvdjhvawjhdvajwddabwdjkbakjwbdkjbbdkbakjdbakdbkabdbad';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  backgroundColor: Colors.black,
-  appBar: AppBar(
-    backgroundColor: Colors.transparent,
-    elevation: 0,
-    centerTitle: false, // Setel ke false agar title tidak berada di tengah
-    title: Row(
-      children: [
-        Text(
-          'FILMIN',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
-      ],
-    ),
-  ),
-  body: Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white,
-          ),
-          child: CircleAvatar(
-            radius: 100, // Ukuran lingkaran profil
-            backgroundImage: NetworkImage('images/logo.jpg'),
-          ),
-        ),
-            SizedBox(height: 22),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: false,
+        title: Row(
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.menu, // Ikon garis tiga (hamburger)
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // Tambahkan fungsi yang ingin dijalankan saat tombol hamburger ditekan
+              },
+            ),
+            SizedBox(width: 8), // Jarak antara ikon dan teks 'FILMIN'
             Text(
-              fullName,
+              'FILMIN',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 20,
               ),
             ),
-            SizedBox(height: 15),
-            Text(
-              email,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16
-              ),
-            ),
-            SizedBox(height: 24),
-            ElevatedButton(
-
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.amber,
-              ),
-              child: Text(
-
-                'Edit Profil',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(height: 18),
-            Align(
-
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'About Me',
-
-                style: TextStyle(
-                  
-                  color: Colors.white,
-
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                aboutMe,
-                style: TextStyle(
-
-                  color: Colors.white,
-                  fontSize: 22,
-                ),
-              ),
-            ),
-            SizedBox(height: 22),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'joined 30 oktober 2023',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 19,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                      onPrimary: Colors.black,
-                    ),
-                    child: Text('Logout'),
-                  ),
-                ),
-              ],
-            ),
           ],
         ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search, // Ikon pencarian (search)
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // Tambahkan fungsi yang ingin dijalankan saat tombol pencarian ditekan
+            },
+          ),
+        ],
       ),
     );
   }
